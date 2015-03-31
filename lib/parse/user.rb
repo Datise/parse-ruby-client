d9eb8f309743ddfa0b9e550be87f0285a5174810
Datise <donald.ballard@hotmail.com>
2015-03-31 13:53:49 -0700

    WHAT

--- lib/parse/user.rb
+++ lib/parse/user.rb
@@ -31,7 +31,7 @@ module Parse
     end
 
     def self.update(data = nil)
-      body = {
+      body = {"username" => data[:username],
         "password" => data[:password].to_s,
         "field"=>data[:data] }
       objectId = data[:objectId]
